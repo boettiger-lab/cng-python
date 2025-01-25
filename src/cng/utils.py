247a4042c4ddca3f5f1b66d6a9e6808eea3cbb6e
Carl Boettiger <cboettig@gmail.com>
2025-01-25 01:29:52 +0000

    pmtiles and boto3

--- src/cng/utils.py
+++ src/cng/utils.py
@@ -1,6 +1,7 @@
 import os
 import ibis
-
+import subprocess
+import re
 
 
 # DuckDB S3 authentication helpers
@@ -36,6 +37,101 @@ def source_secrets(con,
                    endpoint = "data.source.coop",
                    bucket = "cboettig"):
     set_secrets(con, key, secret, endpoint = endpoint, bucket = bucket)
+
+
+# define these as chainable methods?
+
+def to_geojson(input_file, 
+               output_file = "/tmp/tmp.geojson", 
+               con = ibis.duckdb.connect(extensions=["spatial"])):
+
+    # accept a path and create a ibis table:
+    if not isinstance(input_file, ibis.expr.types.relations.Table):
+          input_file = con.read_parquet(input_file)
+    
+    query = ibis.to_sql(input_file)
+    con.raw_sql(f'''
+      COPY ({query}) TO '{output_file}' 
+      WITH (FORMAT GDAL, DRIVER 'GeoJSON', LAYER_CREATION_OPTIONS 'WRITE_BBOX=YES');
+    ''')
+    return(output_file)
+
+
+def to_pmtiles(input_file, 
+               output_file = None, 
+               max_zoom = 12,
+               overwrite = True,
+               options = ["--drop-densest-as-needed", "--extend-zooms-if-still-dropping"]
+
+):
+    # check or convert to local geojson?
+    # check crs?
+    # check valid?
+
+    if output_file is None:
+        output_file = re.sub(r"\..+$", ".pmtiles", input_file)
+
+    # Ensure Tippecanoe is installed
+    if subprocess.call(["which", "tippecanoe"], stdout=subprocess.DEVNULL) != 0:
+        raise RuntimeError("Tippecanoe is not installed or not in PATH")
+
+    # Construct the Tippecanoe command
+    command = [
+        "tippecanoe",
+        "-o", output_file,
+        "-z", str(max_zoom)]
+
+    command.extend(options)
+
+    if overwrite:
+        command.extend(["--force"])
+
+    command.extend([input_file])
+
+    # Run Tippecanoe
+    try:
+        subprocess.run(command, check=True)
+        print(f"Successfully generated PMTiles file: {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Tippecanoe: {e}")
+    
+    return(output_file)
+
+
+# kinda silly.  boto3 doesn't do sync, maybe we just awscli wrap
+# would be nice if source.coop supported minio client library
+import boto3
+from botocore.client import Config
+import streamlit as st
+def s3_cp(source, 
+          dest, 
+          profile = "minio",
+          key = st.secrets["MINIO_KEY"],
+          secret = st.secrets["MINIO_SECRET"]):
+
+    # assume dest is an s3_path?
+    match = re.match(r'^s3://([^/]+)/(.*)$', dest)
+    bucket, object_name = match.groups()
+    if profile == "minio":
+        s3 = boto3.client(
+            "s3",
+            endpoint_url = "https://minio.carlboettiger.info",
+            aws_access_key_id = os.getenv("MINIO_KEY", key), 
+            aws_secret_access_key = os.getenv("MINIO_SECRET", secret),
+            config=Config(s3={'addressing_style': 'path'})
+        )
+        s3.upload_file(source, bucket, object_name)
+    if profile == "sc":
+        s3 = boto3.client("s3",
+            endpoint_url = "https://data.source.coop",
+            aws_access_key_id = os.getenv("SOURCE_KEY", key), 
+            aws_secret_access_key = os.getenv("SOURCE_SECRET", secret),
+            config=Config(s3={'addressing_style': 'path', 
+                              'multipart_threshold': '44MB'})
+        )
+    s3.upload_file(source, bucket, object_name)
+    
+
 
 
 ## kinda silly wraper to minio.Minio but provides my defaults.  Sadly also not working with source.coop yet
@@ -144,7 +240,9 @@ def terrain_style(key = os.getenv('MAPTILER_KEY'), exaggeration = 1):
 
 # enable ibis to use built-in function from the h3 extension
 
-
+@ibis.udf.scalar.builtin
+def h3_polygon_wkt_to_cells_string (geom, int) -> str:
+    ...
 
 import ibis.expr.datatypes as dt
 # usage: t.mutate(geom_valid = ST_MakeValid(t.geom))

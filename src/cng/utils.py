4ed05c06efc3d91e903b9c96f99f00e077ebdb03
Carl Boettiger <cboettig@gmail.com>
2025-02-16 05:05:16 +0000

    refactor h3

--- src/cng/utils.py
+++ src/cng/utils.py
@@ -250,28 +250,6 @@ def hf_upload(file, path, repo_id = "boettiger-lab/ca-30x30", repo_type = "datas
             repo_type=repo_type, 
         )
         
-# enable ibis to use built-in function from the h3 extension
-
-@ibis.udf.scalar.builtin
-def h3_polygon_wkt_to_cells_string (geom, int) -> str:
-    ...
-
-import ibis.expr.datatypes as dt
-# usage: t.mutate(geom_valid = ST_MakeValid(t.geom))
-@ibis.udf.scalar.builtin
-def ST_MakeValid(geom) -> dt.geometry:
-    ...
-
-@ibis.udf.scalar.builtin
-def h3_cell_to_boundary_wkt	(array) -> str:
-    ...
-
-@ibis.udf.scalar.builtin
-def h3_latlng_to_cell(lat: float, lng: float, zoom: int) -> int:
-    ...
-@ibis.udf.scalar.builtin
-def hex(array) -> str:
-    ...
 
 
 

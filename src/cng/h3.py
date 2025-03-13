96057e174006692806b3399483c21213a34e3ca8
Carl Boettiger <cboettig@gmail.com>
2025-03-13 20:55:07 +0000

    h3 utils :see_no_evil:

--- src/cng/h3.py
+++ src/cng/h3.py
@@ -1,5 +1,32 @@
 # enable ibis to use built-in function from the h3 extension
 
+
+def to_h3j (df, path):
+    cols = df.columns
+    sql = ibis.to_sql(df)
+    f'''
+        COPY (
+          WITH t1 AS ({sql})
+          SELECT json_group_array(struct_pack({cols}))
+          AS cells
+          FROM t1
+        ) TO '{path}' (FORMAT JSON)
+    '''
+
+def geom_to_cell (tbl, zoom = 3):
+   sql = f'''
+    WITH t1 AS (
+      SELECT *, ST_Dump(geom) AS geom 
+      FROM {tbl}
+    ) 
+    SELECT *,
+           h3_polygon_wkt_to_cells_string(UNNEST(geom).geom, {zoom}) AS h{zoom}
+    FROM t1
+    '''
+
+
+# .to_parquet(f"s3://public-biodiversity/pad-us-4/pad-h3-z{zoom}.parquet")
+
 @ibis.udf.scalar.builtin
 def h3_polygon_wkt_to_cells_string (geom, int) -> str:
     ...

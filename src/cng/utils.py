ab8969a32b7256dcdeaca3a262f39710b0b23a5d
Carl Boettiger <cboettig@berkeley.edu>
2025-11-14 19:49:35 -0800

    deps

--- src/cng/utils.py
+++ src/cng/utils.py
@@ -112,43 +112,6 @@ def to_pmtiles(input_file,
     return(output_file)
 
 
-# kinda silly.  boto3 doesn't do sync, maybe we just awscli wrap
-# would be nice if source.coop supported minio client library
-import boto3
-from botocore.client import Config
-import streamlit as st
-def s3_cp(source, 
-          dest, 
-          profile = "minio",
-          key = "", #st.secrets["MINIO_KEY"],
-          secret = "", #st.secrets["MINIO_SECRET"]
-          ):
-
-    # assume dest is an s3_path?
-    match = re.match(r'^s3://([^/]+)/(.*)$', dest)
-    bucket, object_name = match.groups()
-    if profile == "minio":
-        s3 = boto3.client(
-            "s3",
-            endpoint_url = "https://minio.carlboettiger.info",
-            aws_access_key_id = os.getenv("MINIO_KEY", key), 
-            aws_secret_access_key = os.getenv("MINIO_SECRET", secret),
-            config=Config(s3={'addressing_style': 'path'})
-        )
-        s3.upload_file(source, bucket, object_name)
-    if profile == "sc":
-        s3 = boto3.client("s3",
-            endpoint_url = "https://data.source.coop",
-            aws_access_key_id = os.getenv("SOURCE_KEY", key), 
-            aws_secret_access_key = os.getenv("SOURCE_SECRET", secret),
-            config=Config(s3={'addressing_style': 'path', 
-                              'multipart_threshold': '44MB'})
-        )
-    s3.upload_file(source, bucket, object_name)
-    
-
-
-
 ## kinda silly wraper to minio.Minio but provides my defaults.  Sadly also not working with source.coop yet
 import minio
 def s3_client(key = os.getenv("MINIO_KEY", ""), 
@@ -164,24 +127,6 @@ def duckdb_install_h3():
     db = duckdb.connect()
     db.install_extension("h3", repository = "community")
     db.close()
-
-
-
-# upload file to huggingface
-from huggingface_hub import HfApi, login
-def hf_upload(file, path, repo_id = "boettiger-lab/ca-30x30", repo_type = "dataset", key = None):
-    if not key:
-        key = st.secrets["HF_TOKEN"]
-    login(key)
-    api = HfApi()
-    info = api.upload_file(
-            path_or_fileobj=path,
-            path_in_repo=file,
-            repo_id=repo_id,
-            repo_type=repo_type, 
-        )
-        
-
 
 
 
